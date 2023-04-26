6dbe0085e2038573eae33428c34b7e20dc47f4e9
yotsai <r8613266@gmail.com>
2023-04-26 21:33:45 +0800

    fix: 修改index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -2,33 +2,33 @@ const lib  = require('.')
 
 jest.setTimeout(30000);
 
-// test('sync funciotn test', async () => {
-//     let checkVal
-//     let runTimesA = 0
-//     let runTimesB = 0  
+test('sync funciotn test ---- this will pass', async () => {
+    let checkVal
+    let runTimesA = 0
+    let runTimesB = 0  
 
-//     // sync loop A
-//     lib.loop(function(runTimes){
-//         console.log("sync loop A", runTimes)
-//         checkVal = 0
-//         expect(checkVal).toBe(0)
-//     }, 10)
+    // sync loop A
+    lib.loop(function(runTimes){
+        console.log("sync loop A", runTimes)
+        checkVal = 0
+        expect(checkVal).toBe(0)
+    }, 10)
 
-//     // sync loop B
-//     lib.loop(function(runTimes){
-//         console.log("sync loop B", runTimes)
-//         checkVal = 1
+    // sync loop B
+    lib.loop(function(runTimes){
+        console.log("sync loop B", runTimes)
+        checkVal = 1
 
-//         expect(checkVal).toBe(1)
-//     })
+        expect(checkVal).toBe(1)
+    })
 
-//     await lib.waitFinish()
-// })
+    await lib.waitFinish()
+})
 
 // 由於js內異步方法會被丟掉web api等到時間結束後才放回task queue的特性
 // 因此checkVal有可能會被其他function修改
 // 導致跟原始修改值不一樣
-test('async sync funciotn test', async () => {
+test('async sync funciotn test ---- high probability failed', async () => {
     let checkVal
     // async loop A
     lib.loop(async function(runTimes){
